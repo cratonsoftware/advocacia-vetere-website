dfaa860a6dcd29685105aa3742545e332f88bd11
Luiz Fernando <f3rn.luiz@gmail.com>
2026-04-07 13:03:14 -0300

    refactor(Styles / Pages): atualização e unificação das cores e atualização do hero

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	isMenuOpen = signal<boolean>(false);
+
+	toggleMenu = () => this.isMenuOpen.update((value) => !value);
+}
