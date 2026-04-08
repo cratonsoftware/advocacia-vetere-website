106188f17d440da7837a673369fe6a6d884a3baf
Luiz Fernando <f3rn.luiz@gmail.com>
2026-04-08 19:01:04 -0300

    refactor(Blog Preview / Contato / Home / Hero): revisão geral dos estilos

--- src/app/features/blog-preview/blog-preview.component.ts
+++ src/app/features/blog-preview/blog-preview.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BlogPost } from '../../core/models/blog.model';
+import { BlogService } from '../../core/models/services/blog.service';
 
 @Component({
 	selector: 'app-blog-preview',
 	templateUrl: './blog-preview.component.html',
+	imports: [CommonModule, RouterLink],
 })
-export class BlogPreviewComponent {}
+export class BlogPreviewComponent implements OnInit {
+	private blogService = inject(BlogService);
+	latestArticles: BlogPost[] = [];
+
+	ngOnInit(): void {
+		this.blogService.getLatestArticles(3).subscribe((articles) => {
+			this.latestArticles = articles;
+		});
+	}
+}
